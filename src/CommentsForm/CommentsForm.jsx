81308756467b93e781485592bee593115b49feb4
CJ Simon <cjsimon250@outlook.com>
2023-02-25 18:02:22 -0600

    finsihed the comment and Suppport component

--- src/CommentsForm/CommentsForm.jsx
+++ src/CommentsForm/CommentsForm.jsx
@@ -1,5 +1,36 @@
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+
 function CommentsForm() {
-  return <h1>COMMENTS</h1>;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [comment, setComment] = useState("");
+
+  //function to send input to reducer and route
+  //user to the next form
+  let handleSubmit = () => {
+    dispatch({
+      type: "ADD_COMMENTS",
+      payload: comment,
+    });
+
+    history.push("/review");
+  };
+  return (
+    <>
+      <h1>How are you feeling today? </h1>
+
+      <input
+        type="text"
+        placeholder="Comment"
+        onChange={(evt) => {
+          setComment(evt.target.value);
+        }}
+      ></input>
+      <button onClick={handleSubmit}>Next</button>
+    </>
+  );
 }
 
 export default CommentsForm;
--- src/SupportForm/SupportForm.jsx
+++ src/SupportForm/SupportForm.jsx
@@ -1,4 +1,41 @@
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+
 function SupportForm() {
-  return <h1>SUPPORT</h1>;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [rating, setRating] = useState(0);
+
+  //function to send input to reducer and route
+  //user to the next form
+  let handleSubmit = () => {
+    dispatch({
+      type: "ADD_SUPPORT",
+      payload: Number(rating),
+    });
+
+    history.push("/comments");
+  };
+  return (
+    <>
+      <h1>How supported did you feel today?</h1>
+      <p>5 being awesome and 1 being not great</p>
+      <select
+        onChange={(evt) => {
+          setRating(evt.target.value);
+        }}
+      >
+        <option value="">Please choose an option</option>
+        <option value="5">5</option>
+        <option value="4">4</option>
+        <option value="3">3</option>
+        <option value="2">2</option>
+        <option value="1">1</option>
+      </select>
+      <button onClick={handleSubmit}>Next</button>
+    </>
+  );
 }
+
 export default SupportForm;
