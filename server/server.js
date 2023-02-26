fe021104055596dd1637bfc11afa0bcbff894931
CJ Simon <cjsimon250@outlook.com>
2023-02-25 19:48:35 -0600

    wrote reviewSubmissions component to display previous ansers to all of the questions

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('build'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("build"));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 
+const submissionRouter = require("./routes/submission.router");
+app.use("/submissions", submissionRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
-    console.log('Listening on port: ', PORT);
+  console.log("Listening on port: ", PORT);
 });
--- src/ReviewSubmissions/ReviewSubmissions.jsx
+++ src/ReviewSubmissions/ReviewSubmissions.jsx
@@ -1,5 +1,44 @@
-function ReviewSubmission() {
-  return <h1>REVIEW</h1>;
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import axios from "axios";
+
+import { useSelector } from "react-redux";
+
+function ReviewSubmissions() {
+  const history = useHistory();
+
+  let answers = useSelector((store) => store.responses);
+
+  //function to handle submission to database & routing
+  function handleSubmit() {
+    axios
+      .post("/submissions", answers)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        alert("error in handleSubmit", err);
+      });
+
+    history.push("/");
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>Review </h1>
+      </div>
+
+      <button
+        onClick={() => {
+          handleSubmit;
+        }}
+      >
+        Submit
+      </button>
+    </div>
+  );
 }
 
-export default ReviewSubmission;
+export default ReviewSubmissions;
